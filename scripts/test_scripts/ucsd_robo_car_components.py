439f50858071747453df624eb0a3f137cc4d4c03
Dominic Nightingale <djnighti@ucsd.edu>
2021-08-19 23:39:32 +0000

    Update ucsd_robo_car_components.py

--- scripts/test_scripts/ucsd_robo_car_components.py
+++ scripts/test_scripts/ucsd_robo_car_components.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
-# import cv2
+import cv2
 # # from adafruit_servokit import ServoKit
-
+vid = cv2.VideoCapture(0)
 
 
 # cap = cv2.VideoCapture(0)
 # cv2.namedWindow('sliders')
 
-# def callback(x):
-#     pass
+def callback(x):
+    pass
 
-# def slider_to_normalized(slider_input):
-#     input_start = 0
-#     input_end = 2000
-#     output_start = -1
-#     output_end = 1
-#     normalized_output = output_start + (slider_input - input_start) * (
-#             (output_end - output_start) / (input_end - input_start))
-#     return normalized_output
+def slider_to_normalized(slider_input):
+    input_start = 0
+    input_end = 2000
+    output_start = -1
+    output_end = 1
+    normalized_output = output_start + (slider_input - input_start) * (
+            (output_end - output_start) / (input_end - input_start))
+    return normalized_output
 
 # # THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
-# steer_left = 0
-# steer_straight = 1000
-# steer_right = 2000
+steer_left = 0
+steer_straight = 1000
+steer_right = 2000
 
 # #THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
-# throttle_reverse = 0
-# throttle_neutral = 1100
-# throttle_forward = 2000
+throttle_reverse = 0
+throttle_neutral = 1100
+throttle_forward = 2000
 
-# cv2.createTrackbar('Steering_value', 'sliders', steer_straight, steer_right, callback)
-# cv2.createTrackbar('Throttle_value', 'sliders', throttle_neutral, throttle_forward, callback)
+cv2.createTrackbar('Steering_value', 'sliders', steer_straight, steer_right, callback)
+cv2.createTrackbar('Throttle_value', 'sliders', throttle_neutral, throttle_forward, callback)
 # print("debug 5")
 
 # def test_car():
@@ -72,17 +72,24 @@
 #         test_car()
 #     print("debug 4")
 
-import cv2
+# import cv2
   
   
 # define a video capture object
-vid = cv2.VideoCapture(0)
+
   
 while(True):
       
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
+    steer_input = cv2.getTrackbarPos('Steering_value', 'sliders')
+    throttle_input = cv2.getTrackbarPos('Throttle_value', 'sliders')
+
+    steering_float = slider_to_normalized(steer_input)
+    throttle_float = slider_to_normalized(throttle_input)
+
+    print(f"\rcurrent steering/throttle: {steering_float}, {throttle_float}", end=" ")
   
     # Display the resulting frame
     cv2.imshow('frame', frame)
