09054e7f0ac0a481adbbde4a0bbb6907b40e98a2
Dominic Nightingale <djnighti@ucsd.edu>
2021-08-19 23:32:19 +0000

    Update ucsd_robo_car_components.py

--- scripts/test_scripts/ucsd_robo_car_components.py
+++ scripts/test_scripts/ucsd_robo_car_components.py
@@ -33,59 +33,37 @@ cv2.createTrackbar('Steering_value', 'sliders', steer_straight, steer_right, cal
 cv2.createTrackbar('Throttle_value', 'sliders', throttle_neutral, throttle_forward, callback)
 print("debug 5")
 
-class TestingCar:
-    def __init__(self):
-        self.place = 1
-        # self.kit = ServoKit(channels=16)
-        # self.cap = cv2.VideoCapture(0)
-        # self.slider_window_name = cv2.namedWindow('sliders')
-        
-        #THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
-        # self.steer_left = 0
-        # self.steer_straight = 1000
-        # self.steer_right = 2000
-
-        # #THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
-        # self.throttle_reverse = 0
-        # self.throttle_neutral = 1100
-        # self.throttle_forward = 2000
-
-        # create trackbars for motor control
-        # self.steering_track_bar = cv2.createTrackbar('Steering_value', 'sliders', self.steer_straight, self.steer_right, callback)
-        # self.throttle_track_bar = cv2.createTrackbar('Throttle_value', 'sliders', self.throttle_neutral, self.throttle_forward, callback)
-        print("debug 2")
-
-    def test_car(self):
-        while(True):
-            try:  
-                # Capture the camera feed frame by frame
-                # ret, frame = self.cap.read()
-                ret, frame = cap.read()
-                steer_input = cv2.getTrackbarPos('Steering_value', 'sliders')
-                throttle_input = cv2.getTrackbarPos('Throttle_value', 'sliders')
+def test_car(self):
+    while(True):
+        try:  
+            # Capture the camera feed frame by frame
+            # ret, frame = self.cap.read()
+            ret, frame = cap.read()
+            steer_input = cv2.getTrackbarPos('Steering_value', 'sliders')
+            throttle_input = cv2.getTrackbarPos('Throttle_value', 'sliders')
                 
-                # re-map input values [0,2000] ---> [-1,1] for BOTH throttle and steering
-                steering_float = slider_to_normalized(steer_input)
-                throttle_float = slider_to_normalized(throttle_input)
+            # re-map input values [0,2000] ---> [-1,1] for BOTH throttle and steering
+            steering_float = slider_to_normalized(steer_input)
+            throttle_float = slider_to_normalized(throttle_input)
 
-                # print(f"\rcurrent steering/throttle: {steering_float}, {throttle_float}", end=" ")
+            # print(f"\rcurrent steering/throttle: {steering_float}, {throttle_float}", end=" ")
 
-                # Publish re-mapped throttle/steering values
-                # self.kit.servo[1].angle = steering_float # steering servo is on channel 1
-                # self.kit.continuous_servo[2].throttle = throttle_float # DC motor is on channel 2
+            # Publish re-mapped throttle/steering values
+            # kit.servo[1].angle = steering_float # steering servo is on channel 1
+            # kit.continuous_servo[2].throttle = throttle_float # DC motor is on channel 2
 
-                # Display the resulting frame
-                cv2.imshow('frame', frame)
-            except KeyboardInterrupt:  
-                break
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
+        except KeyboardInterrupt:  
+            break
         
-        # After the loop release the cap object
-        cap.release()
-        # self.cap.release()
-        # Destroy all the windows
-        cv2.destroyAllWindows()
+    # After the loop release the cap object
+    cap.release()
+    # self.cap.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     print("debug 1")
-    TestingCar().test_car()
+    test_car()
     print("debug 4")
