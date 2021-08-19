70318e4e7ba35469e91512800e4e2eca87dd039d
Dominic Nightingale <djnighti@ucsd.edu>
2021-08-19 23:10:29 +0000

    Update ucsd_robo_car_components.py

--- scripts/test_scripts/ucsd_robo_car_components.py
+++ scripts/test_scripts/ucsd_robo_car_components.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import cv2
-from adafruit_servokit import ServoKit
+# from adafruit_servokit import ServoKit
 
-kit = ServoKit(channels=16)
-cv2.namedWindow('sliders')
+
+
+# cap = cv2.VideoCapture(0)
+# cv2.namedWindow('sliders')
 
 def callback(x):
     pass
@@ -18,27 +20,38 @@ def slider_to_normalized(slider_input):
     return normalized_output
 
 #THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
-steer_left = 0
-steer_straight = 1000
-steer_right = 2000
+# steer_left = 0
+# steer_straight = 1000
+# steer_right = 2000
 
-#THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
-throttle_reverse = 0
-throttle_neutral = 1100
-throttle_forward = 2000
+# #THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
+# throttle_reverse = 0
+# throttle_neutral = 1100
+# throttle_forward = 2000
 
+# cv2.createTrackbar('Steering_value', 'sliders', steer_straight, steer_right, callback)
+# cv2.createTrackbar('Throttle_value', 'sliders', throttle_neutral, throttle_forward, callback)
 
-cv2.createTrackbar('Steering_value', 'sliders', steer_straight, steer_right, callback)
-cv2.createTrackbar('Throttle_value', 'sliders', throttle_neutral, throttle_forward, callback)
-
-
-kit.servo[1].angle = 90 # steering servo is on channel 1
-kit.continuous_servo[2].throttle = 0.1 # DC motor is on channel 2
-cap = cv2.VideoCapture(0)
 
 class TestingCar:
     def __init__(self):
-        self.dummy = None
+        self.kit = ServoKit(channels=16)
+        self.cap = cv2.VideoCapture(0)
+        self.cv2.namedWindow('sliders')
+        
+        #THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
+        self.steer_left = 0
+        self.steer_straight = 1000
+        self.steer_right = 2000
+
+        #THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
+        self.throttle_reverse = 0
+        self.throttle_neutral = 1100
+        self.throttle_forward = 2000
+
+        # create trackbars for motor control
+        self.cv2.createTrackbar('Steering_value', 'sliders', steer_straight, steer_right, callback)
+        self.cv2.createTrackbar('Throttle_value', 'sliders', throttle_neutral, throttle_forward, callback)
 
     def test_car(self):
         while(True):
@@ -48,9 +61,15 @@ class TestingCar:
                 steer_input = cv2.getTrackbarPos('Steering_value', 'sliders')
                 throttle_input = cv2.getTrackbarPos('Throttle_value', 'sliders')
                 
+                # re-map input values [0,2000] ---> [-1,1] for BOTH throttle and steering
                 steering_float = Steering_sensitivity*slider_to_normalized(steer_input)
-                throttle_floaT = slider_to_normalized(throttle_input)
+                throttle_float = slider_to_normalized(throttle_input)
 
+                print(f"\rcurrent steering/throttle: {steering_float}, {throttle_float}", end=" ")
+
+                # Publish re-mapped throttle/steering values
+                # self.kit.servo[1].angle = steering_float # steering servo is on channel 1
+                # self.kit.continuous_servo[2].throttle = throttle_float # DC motor is on channel 2
 
                 # Display the resulting frame
                 cv2.imshow('frame', frame)
@@ -63,3 +82,4 @@ class TestingCar:
         cv2.destroyAllWindows()
 
 if __name__ == '__main__':
+    TestingCar().test_car
