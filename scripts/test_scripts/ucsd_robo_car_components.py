d3f1275d8ed2d0ea7984f38870e18aba5c26b9d5
Dominic Nightingale <djnighti@ucsd.edu>
2021-08-20 16:31:36 +0000

    Update ucsd_robo_car_components.py

--- scripts/test_scripts/ucsd_robo_car_components.py
+++ scripts/test_scripts/ucsd_robo_car_components.py
@@ -5,6 +5,7 @@ from adafruit_servokit import ServoKit
 
 vid = cv2.VideoCapture(0)
 cv2.namedWindow('frame')
+kit = ServoKit(channels=16)
 
 def callback(x):
     pass
@@ -22,6 +23,7 @@ def slider_to_normalized(slider_input):
 steer_left = 0
 steer_straight = 1000
 steer_right = 2000
+degree_90 = 90
 
 # #THESE VALUES ARE ALL SCALED FOR FINE TUNINING. ACTUAL VALUES RANGE FROM [-1,1]
 throttle_reverse = 0
@@ -35,24 +37,26 @@ while(True):
       
     # Capture the video frame by frame
     ret, frame = vid.read()
+
+    # Get slider values
     steer_input = cv2.getTrackbarPos('Steering_value', 'frame')
     throttle_input = cv2.getTrackbarPos('Throttle_value', 'frame')
 
+    # Re-map input values
     steering_float = slider_to_normalized(steer_input)
     throttle_float = slider_to_normalized(throttle_input)
 
-    kit.servo[1].angle = steering_float # steering servo is on channel 1
+    # Publish values to actuators
+    angle_delta = steering_float * degree_90  # difference in degrees from the center 90 degrees
+    kit.servo[1].angle = degree_90 + angle_delta # steering servo is on channel 1 (0:left, 90:straight, 180:right)
     kit.continuous_servo[2].throttle = throttle_float # DC motor is on channel 2
-
 
     print(f"\r[steering, throttle]: [{round(steering_float, 2)}, {round(throttle_float, 2)}]", end=" ")
     
     # Display the resulting frame
     cv2.imshow('frame', frame)
       
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
+    # the 'q' button is set as the quitting button you may use any desired button of your choice
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
   
