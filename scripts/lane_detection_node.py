20dad57e3d1a0773c3ce020f2f1a7ded8a9061ce
Dominic Nightingale <djnighti@ucsd.edu>
2021-08-21 21:27:43 +0000

    Update lane_detection_node.py

--- scripts/lane_detection_node.py
+++ scripts/lane_detection_node.py
@@ -24,6 +24,7 @@ class LaneDetection:
         self.init_node = rospy.init_node(LANE_DETECTION_NODE_NAME, anonymous=False)
         self.camera_subscriber = rospy.Subscriber(CAMERA_TOPIC_NAME, Image, self.locate_centroid)
         self.centroid_error_publisher = rospy.Publisher(CENTROID_TOPIC_NAME, Float32, queue_size=1)
+        self.centroid_error = Float32()
         self.bridge = CvBridge()
 
         # Getting ROS parameters set from calibration Node
