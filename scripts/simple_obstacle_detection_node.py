45ad0cc182e14518b6b4ed7f81fd5807a632e37c
sbenetz <sbenetz@ucsd.edu>
2021-12-07 17:17:06 -0800

    we love self

--- scripts/simple_obstacle_detection_node.py
+++ scripts/simple_obstacle_detection_node.py
@@ -64,7 +64,7 @@ class ObstacleDetection:
             obstacle_detected = 0.0
 
             # Publish ROS message
-            self.obstacle_info.append(min_distance_data)
+            self.obstacle_info.append(self.min_distance_data)
             self.obstacle_info.append(normalized_angle)
             self.obstacle_info.append(obstacle_detected)
             self.obstacle_pub.publish(self.obstacle_info)
