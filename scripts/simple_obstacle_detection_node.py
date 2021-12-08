9298bb3691494f2105d92c0babe5f9dc09991432
Andrew Liang <aliang@ucsd.edu>
2021-12-07 19:03:56 -0800

    eahwahewahew

--- scripts/simple_obstacle_detection_node.py
+++ scripts/simple_obstacle_detection_node.py
@@ -44,7 +44,7 @@ class ObstacleDetection:
         min_distance = min(range_values)
         min_angle_index = range_values.index(min(range_values))
         min_angle = angle_values[min_angle_index]
-        obstacle_info = Float32MultiArray()
+        obstacle_info = []
         #rospy.loginfo("Obstacle Detected:" + str(min_distance))
         if self.max_distance_tolerance >= abs(min_distance) >= self.min_distance_tolerance:
             angle_rad = (min_angle * math.pi) / 180
